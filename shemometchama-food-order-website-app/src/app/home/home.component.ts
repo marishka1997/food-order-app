cf2d5d94d50c033153ef9b9f00816d09c3379f27
Mariam Gasparian <m.gasparian11@gmail.com>
2022-07-03 22:23:56 +0400

    add getAllFoodByTag() method in service and create tag model for holding tags

--- shemometchama-food-order-website-app/src/app/home/home.component.ts
+++ shemometchama-food-order-website-app/src/app/home/home.component.ts
@@ -16,6 +16,8 @@ foods: Foods[] = [];
 this.route.params.subscribe(params => {
   if (params['searchItem'])
   this.foods = this.foodservice.getAll().filter(food => food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
+  else if (params['tag'])
+  this.foods = this.foodservice.getAllFoodByTag(params['tag'])
   else
   this.foods = this.foodservice.getAll();
 })
--- shemometchama-food-order-website-app/src/app/services/food/food.service.ts
+++ shemometchama-food-order-website-app/src/app/services/food/food.service.ts
@@ -2,47 +2,50 @@ import { Injectable } from '@angular/core';
 import { Foods } from 'src/app/shared/models/food';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodService {
+  constructor() {}
 
-  constructor() { }
-
+  getAllFoodByTag(tag: string): Foods[] {
+    return tag == 'All'
+      ? this.getAll()
+      : this.getAll().filter((food) => food.tags?.includes(tag));
+  }
   getAll(): Foods[] {
-return [
-{
-  id: 1,
-  name: 'Kalakuri Khinkali',
-  cookTime: '30-40',
-  price: 10,
-  favourite: false,
-  origins: ['Georgia'],
-  star: 4.5,
-  imageUrl: `assets/kalakuri-khinkali.jpg`,
-  tags: ['Khinkali']
-},
-{
-  id: 2,
-  name: 'Kalakuri Khinkali',
-  cookTime: '30-40',
-  price: 10,
-  favourite: false,
-  origins: ['Georgia'],
-  star: 3.2,
-  imageUrl: `assets/kalakuri-khinkali.jpg`,
-  tags: ['Khinkali']
-}
+    return [
+      {
+        id: 1,
+        name: 'Kalakuri Khinkali',
+        cookTime: '30-40',
+        price: 10,
+        favourite: false,
+        origins: ['Georgia'],
+        star: 4.5,
+        imageUrl: `assets/kalakuri-khinkali.jpg`,
+        tags: ['Khinkali'],
+      },
+      {
+        id: 2,
+        name: 'Kalakuri Khinkali',
+        cookTime: '30-40',
+        price: 10,
+        favourite: false,
+        origins: ['Georgia'],
+        star: 3.2,
+        imageUrl: `assets/kalakuri-khinkali.jpg`,
+        tags: ['Khinkali'],
+      },
 
-
-  // `assets/khinkali/kalakuri-khinkali.jpg`,
-  // `assets/khinkali/kalakuri-khinkali.jpg`,
-  // `assets/khinkali/kalakuri-khinkali.jpg`,
-  // `assets/khinkali/kalakuri-khinkali.jpg`,
-  // `assets/khinkali/kalakuri-khinkali.jpg`,
-  // `assets/khinkali/kalakuri-khinkali.jpg`,
-  // `assets/khinkali/kalakuri-khinkali.jpg`,
-  // `assets/khinkali/kalakuri-khinkali.jpg`,
-  // `assets/khinkali/kalakuri-khinkali.jpg`,
-];
+      // `assets/khinkali/kalakuri-khinkali.jpg`,
+      // `assets/khinkali/kalakuri-khinkali.jpg`,
+      // `assets/khinkali/kalakuri-khinkali.jpg`,
+      // `assets/khinkali/kalakuri-khinkali.jpg`,
+      // `assets/khinkali/kalakuri-khinkali.jpg`,
+      // `assets/khinkali/kalakuri-khinkali.jpg`,
+      // `assets/khinkali/kalakuri-khinkali.jpg`,
+      // `assets/khinkali/kalakuri-khinkali.jpg`,
+      // `assets/khinkali/kalakuri-khinkali.jpg`,
+    ];
   }
 }
