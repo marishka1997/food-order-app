fd7fbe73848a0c4ec01eeb1a8e2be9e4fac00837
Mariam Gasparian <m.gasparian11@gmail.com>
2022-07-03 23:30:42 +0400

    styling categories/tags

--- shemometchama-food-order-website-app/src/app/home/home.component.ts
+++ shemometchama-food-order-website-app/src/app/home/home.component.ts
@@ -7,22 +7,25 @@ import { identifierName } from '@angular/compiler';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-foods: Foods[] = [];
-  constructor(private foodservice: FoodService, private route: ActivatedRoute) { }
+  foods: Foods[] = [];
+  constructor(
+    private foodservice: FoodService,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-this.route.params.subscribe(params => {
-  if (params['searchItem'])
-  this.foods = this.foodservice.getAll().filter(food => food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
-  else if (params['tag'])
-  this.foods = this.foodservice.getAllFoodByTag(params['tag'])
-  else
-  this.foods = this.foodservice.getAll();
-})
-
-
+    this.route.params.subscribe((params) => {
+      if (params['searchItem'])
+        this.foods = this.foodservice
+          .getAll()
+          .filter((food) =>
+            food.name.toLowerCase().includes(params['searchItem'].toLowerCase())
+          );
+      else if (params['tag'])
+        this.foods = this.foodservice.getAllFoodByTag(params['tag']);
+      else this.foods = this.foodservice.getAll();
+    });
   }
-
 }
