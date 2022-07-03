ee960660c6cb3bc7e63482b816903fdfbd76d3e6
Mariam Gasparian <m.gasparian11@gmail.com>
2022-07-03 18:37:06 +0400

    use ActivatedRoute in home.component for searchItem

--- shemometchama-food-order-website-app/src/app/home/home.component.ts
+++ shemometchama-food-order-website-app/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/food';
 import { StarRatingComponent } from 'ng-starrating';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,7 +10,7 @@ import { StarRatingComponent } from 'ng-starrating';
 })
 export class HomeComponent implements OnInit {
 foods: Foods[] = [];
-  constructor(private foodservice: FoodService) { }
+  constructor(private foodservice: FoodService, private router: ActivatedRoute) { }
   ngOnInit(): void {
     this.foods = this.foodservice.getAll();
   }
