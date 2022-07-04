a5e54cb26a6a26939daba21522bab712435d5ee9
Mariam Gasparian <m.gasparian11@gmail.com>
2022-07-04 17:12:01 +0400

    create foodpage component and app-routing changes

--- shemometchama-food-order-website-app/src/app/app.module.ts
+++ shemometchama-food-order-website-app/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './home/home.component';
 import { RatingModule } from 'ng-starrating';
 import { SearchComponent } from './search/search.component';
 import { TagsComponent } from './tags/tags.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { FoodpageComponent } from './foodpage/foodpage.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,8 @@ import { TagsComponent } from './tags/tags.component';
     HomeComponent,
     SearchComponent,
     TagsComponent,
+    CartPageComponent,
+    FoodpageComponent,
   ],
   imports: [
     BrowserModule,
--- shemometchama-food-order-website-app/src/app/services/cart.service.ts
+++ shemometchama-food-order-website-app/src/app/services/cart.service.ts
@@ -1,5 +1,7 @@
+import { isNgTemplate, TemplateBindingParseResult } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Cart } from '../shared/models/Cart';
+import { CartItem } from '../shared/models/CartItem';
 import { Foods } from '../shared/models/food';
 
 @Injectable({
@@ -7,12 +9,23 @@ import { Foods } from '../shared/models/food';
 })
 export class CartService {
   private cart: Cart = new Cart();
- addToCart(food: Foods): void {
-   let cartItem = this.cart.items.find(item => item.food.id === food.id)
-   if (cartItem) {
-     this.changeQuantity(food.id, cartItem.quantity + 1);
-     return;
-   }
-   this.cart.items.push(new cartItem(food));
- }
+  addToCart(food: Foods): void {
+    let cartItem = this.cart.items.find((item) => item.food.id === food.id);
+    if (cartItem) {
+      this.changeQuantity(food.id, cartItem.quantity + 1);
+      return;
+    }
+    this.cart.items.push(new CartItem(food));
+  }
+  removeFromCart(foodId: number): void {
+    this.cart.items = this.cart.items.filter((item) => item.food.id != foodId);
+  }
+  changeQuantity(quantity: number, foodId: number) {
+    let cartItem = this.cart.items.find(item => item.food.id === foodId);
+    if(cartItem) return;
+    cartItem.quantity = quantity;
+  }
+  getCart(): Cart {
+    return this.cart;
+  }
 }
