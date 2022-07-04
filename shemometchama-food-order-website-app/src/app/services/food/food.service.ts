146a440961e218940e459678c311bc0293c1be91
Mariam Gasparian <m.gasparian11@gmail.com>
2022-07-04 18:16:02 +0400

    adding food page component with functionaility without styling

--- shemometchama-food-order-website-app/src/app/services/food/food.service.ts
+++ shemometchama-food-order-website-app/src/app/services/food/food.service.ts
@@ -8,6 +8,12 @@ import { Tag } from 'src/app/shared/models/Tag';
 export class FoodService {
   constructor() {}
 
+  getFoodById(id: number): Foods {
+    return this.getAll().find(food => food.id == id)!;
+  }
+
+
+
   getAllFoodByTag(tag: string): Foods[] {
     return tag == 'All'
       ? this.getAll()
--- shemometchama-food-order-website-app/src/app/tags/tags.component.ts
+++ shemometchama-food-order-website-app/src/app/tags/tags.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/food';
 import { Tag } from '../shared/models/Tag';
@@ -6,14 +6,16 @@ import { Tag } from '../shared/models/Tag';
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
-  styleUrls: ['./tags.component.css']
+  styleUrls: ['./tags.component.css'],
 })
 export class TagsComponent implements OnInit {
-tags: Tag[] = [];
-  constructor(private foodservice: FoodService) { }
+  @Input()
+  foodPageTags?: string[];
+  tags?: Tag[] = [];
+  constructor(private foodservice: FoodService) {}
 
   ngOnInit(): void {
+    if(!this.foodPageTags)
     this.tags = this.foodservice.getAllTag();
   }
-
 }
